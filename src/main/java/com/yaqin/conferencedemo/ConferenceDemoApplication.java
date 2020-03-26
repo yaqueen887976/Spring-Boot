8fda4b04f028fac31a1add27f5a0b68214739528
yaqueen887976 <yaqinwang@email.arizona.edu>
2020-03-25 20:04:31 -0600

    config

--- src/main/java/com/yaqin/conferencedemo/ConferenceDemoApplication.java
+++ src/main/java/com/yaqin/conferencedemo/ConferenceDemoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ConferenceDemoApplication {
-
+	//spring.datasource.url=jdbc:postgresql://localhost:5433/
 	public static void main(String[] args) {
 		SpringApplication.run(ConferenceDemoApplication.class, args);
 	}
