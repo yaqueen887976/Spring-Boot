5c3eee6b2b214c742ae594b357433cb50c29798f
yaqueen887976 <yaqinwang@email.arizona.edu>
2020-04-09 09:58:26 -0600

    new branch commit

---
+++ src/main/java/com/yaqin/fundamental/FundamentalApplication.java
@@ -0,0 +1,13 @@
+package com.yaqin.fundamental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundamentalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FundamentalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yaqin/fundamental/FundamentalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yaqin.fundamental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundamentalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
